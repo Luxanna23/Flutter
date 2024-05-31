f3fb233ee4a45a0569fa8532db3eb265bd95b591
Yasmine <yasmine.amaddah@laplateforme.io>
2024-05-31 10:39:41 +0200

    ok

--- Flutter/exo5.dart
+++ Flutter/exo5.dart
@@ -16,99 +16,38 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 255, 255),
+              Expanded (
+              flex: 1,
+              child: Container(
+                  decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 94, 252, 102),
+                  borderRadius: BorderRadius.circular(10),
                   border: Border.all(
                   width: 4.0,),
                  ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                        child: Text('1-1'),
-                      )
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                              child: Text('1-2'),
-                      )
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                              child: Text('1-3'),
-                      )
-                    ),
-                  ],
-              ),
-              
+              )
             ),
-            Container(
-            child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                        child: Text('2-1'),
-                      )
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                              child: Text('2-2'),
-                      )
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                        width: 4.0,)
-                      ),
-                      child: const Center(
-                        child: Text('2-3'),
-                      )
-                    ),
-                  ],
-              ),
+            Expanded (
+              flex: 2,
+              child: Container(
+                  decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 252, 239, 94),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                  width: 4.0,),
+                 ),
+              )
+            ),
+            Expanded (
+              flex: 1,
+              child: Container(
+                  decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 115, 152, 255),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                  width: 4.0,),
+                 ),
+              )
             ),
             ],
           ),
